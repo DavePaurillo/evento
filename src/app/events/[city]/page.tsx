07315cb26b526f3b20ee82f8c2b694c59eb2484b
DavePaurillo <paurillo.dave@gmail.com>
2024-12-23 01:23:02 +0800

    feat: direct fetch from a server component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,10 +1,16 @@
+import { EventoEvent } from "@/app/types"
 import H1 from "@/components/h1"
 import React from "react"
 
-export default function Events({
+export default async function Events({
 	params,
 }: Readonly<{ params: { city: string } }>) {
 	const { city } = params
+
+	const response = await fetch(
+		`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+	)
+	const events: EventoEvent[] = await response.json()
 
 	return (
 		<main className='flex flex-col items-center py-24 px-[20px] min-h-[110vh]'>
@@ -13,6 +19,10 @@ export default function Events({
 			) : (
 				<H1>Events in {city.charAt(0).toUpperCase() + city.slice(1)}</H1>
 			)}
+
+			{events.map((event) => {
+				return <div key={event.id}>{event.name}</div>
+			})}
 		</main>
 	)
 }
