be777d85eeafcd196e6418247255494861615b1d
DavePaurillo <paurillo.dave@gmail.com>
2024-12-22 21:55:15 +0800

    feat: created the header | attached the logo using remote url

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,37 @@
 import Link from "next/link"
 import React from "react"
+import Logo from "./logo"
+
+const routes = [
+	{
+		name: "Home",
+		path: "/",
+	},
+	{
+		name: "All Events",
+		path: "/events/all",
+	},
+]
 
 export default function Header() {
 	return (
-		<header>
-			<Link href='/'>Home</Link>
-			<Link href='/events/all'>All Events</Link>
+		<header className='flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9'>
+			<Logo />
+
+			<nav>
+				<ul className='flex gap-6 text-sm'>
+					{routes.map((route) => {
+						return (
+							<li
+								key={route.path}
+								className='text-white/50 hover:text-white transition'
+							>
+								<Link href={route.path}>{route.name}</Link>
+							</li>
+						)
+					})}
+				</ul>
+			</nav>
 		</header>
 	)
 }
