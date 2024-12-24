290995a15b719c4e648342e909fd23b4f83e8e2d
DavePaurillo <paurillo.dave@gmail.com>
2024-12-25 03:28:32 +0800

    perf: unstable_cache to db calls

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import clsx, { ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import prisma from "./db"
 import { notFound } from "next/navigation"
+import { unstable_cache } from "next/cache"
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
@@ -11,35 +12,37 @@ export function capitalizeFirstCharacter(str: string) {
 	return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-export async function getEvents(query: string, value: string, page = 1) {
-	// undefined is used to fetch all events
-	// we cannot use the undefined trick for the totalCount so we created a variable for 2 different queries
-	const events = await prisma.eventoEvent.findMany({
-		where: {
-			[query]: value === "all" ? undefined : capitalizeFirstCharacter(value),
-		},
-		orderBy: {
-			date: "asc",
-		},
-		take: 6,
-		skip: (page - 1) * 6,
-	})
-
-	let totalCount = 0
-	if (value === "all") {
-		totalCount = await prisma.eventoEvent.count()
-	} else {
-		totalCount = await prisma.eventoEvent.count({
+export const getEvents = unstable_cache(
+	async (query: string, value: string, page = 1) => {
+		// undefined is used to fetch all events
+		// we cannot use the undefined trick for the totalCount so we created a variable for 2 different queries
+		const events = await prisma.eventoEvent.findMany({
 			where: {
-				[query]: capitalizeFirstCharacter(value),
+				[query]: value === "all" ? undefined : capitalizeFirstCharacter(value),
 			},
+			orderBy: {
+				date: "asc",
+			},
+			take: 6,
+			skip: (page - 1) * 6,
 		})
+
+		let totalCount = 0
+		if (value === "all") {
+			totalCount = await prisma.eventoEvent.count()
+		} else {
+			totalCount = await prisma.eventoEvent.count({
+				where: {
+					[query]: capitalizeFirstCharacter(value),
+				},
+			})
+		}
+
+		return { events, totalCount }
 	}
+)
 
-	return { events, totalCount }
-}
-
-export async function getEvent(slug: string) {
+export const getEvent = unstable_cache(async (slug: string) => {
 	const event = await prisma.eventoEvent.findUnique({
 		where: {
 			slug: slug,
@@ -51,4 +54,4 @@ export async function getEvent(slug: string) {
 	}
 
 	return event
-}
+})
